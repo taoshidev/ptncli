3d6e79f1d8d979bff152eaf65c23c68166deaeaf
Kenneth Ashley <ken.u.diggitt@gmail.com>
2025-07-10 14:36:49 -0400

    Adds stake list command

--- src/ptncli/commands/stake/__init__.py
+++ src/ptncli/commands/stake/__init__.py
@@ -1,8 +1,10 @@
 import typer
 from .add import add_command
+from .list import list_command
 
 app = typer.Typer(help="Stake management operations")
 
 app.command(name="add")(add_command)
+app.command(name="list")(list_command)
 
 __all__ = ["app"]
